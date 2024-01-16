93cc5e4f54c3a26691116f615531805dc08d6be3
sethvs <sethvs@users.noreply.github.com>
2024-01-16 05:44:10 +0300

    Update attributes

--- attributes/overwrites.ps1
+++ attributes/overwrites.ps1
@@ -5,177 +5,102 @@ $overwrites = @(
         [ordered]@{AttributeID = 5
             AttributeIDHex = '5'
             AttributeName = 'Retired Block Count'
-            BetterValue = ''
-            IsCritical = $null
-            Description = ''
         },
         [ordered]@{AttributeID = 100
             AttributeIDHex = '64'
             AttributeName = 'Total Erase Count'
-            BetterValue = ''
-            IsCritical = $null
-            Description = ''
         },
         [ordered]@{AttributeID = 168
             AttributeIDHex = 'A8'
             AttributeName = 'Min Erase Count'
-            BetterValue = ''
-            IsCritical = $null
-            Description = ''
         },
         [ordered]@{AttributeID = 169
             AttributeIDHex = 'A9'
             AttributeName = 'Max Erase Count'
-            BetterValue = ''
-            IsCritical = $null
-            Description = ''
         },
         [ordered]@{AttributeID = 170
             AttributeIDHex = 'AA'
             AttributeName = 'Unknown SK hynix Attrib'
-            BetterValue = ''
-            IsCritical = $null
-            Description = ''
         },
         [ordered]@{AttributeID = 171
             AttributeIDHex = 'AB'
             AttributeName = 'Program Fail Count'
-            BetterValue = ''
-            IsCritical = $null
-            Description = ''
         },
         [ordered]@{AttributeID = 172
             AttributeIDHex = 'AC'
             AttributeName = 'Erase Fail Count'
-            BetterValue = ''
-            IsCritical = $null
-            Description = ''
         },
         [ordered]@{AttributeID = 173
             AttributeIDHex = 'AD'
             AttributeName = 'Wear Leveling Count'
-            BetterValue = ''
-            IsCritical = $null
-            Description = ''
         },
         [ordered]@{AttributeID = 174
             AttributeIDHex = 'AE'
             AttributeName = 'Unexpect Power Loss Ct'
-            BetterValue = ''
-            IsCritical = $null
-            Description = ''
         },
         [ordered]@{AttributeID = 176
             AttributeIDHex = 'B0'
             AttributeName = 'Unused Rsvd Blk Cnt Tot'
-            BetterValue = ''
-            IsCritical = $null
-            Description = ''
         },
         [ordered]@{AttributeID = 180
             AttributeIDHex = 'B4'
             AttributeName = 'Erase Fail Count'
-            BetterValue = ''
-            IsCritical = $null
-            Description = ''
         },
         [ordered]@{AttributeID = 181
             AttributeIDHex = 'B5'
             AttributeName = 'Non4k Aligned Access'
-            BetterValue = ''
-            IsCritical = $null
-            Description = ''
         },
         [ordered]@{AttributeID = 183
             AttributeIDHex = 'B7'
             AttributeName = 'SATA Downshift Count'
-            BetterValue = ''
-            IsCritical = $null
-            Description = ''
         },
         [ordered]@{AttributeID = 191
             AttributeIDHex = 'BF'
             AttributeName = 'Unknown SK hynix Attrib'
-            BetterValue = ''
-            IsCritical = $null
-            Description = ''
         },
         [ordered]@{AttributeID = 201
             AttributeIDHex = 'C9'
             AttributeName = 'Percent Lifetime Remain'
-            BetterValue = ''
-            IsCritical = $null
-            Description = ''
         },
         [ordered]@{AttributeID = 212
             AttributeIDHex = 'D4'
             AttributeName = 'Phy Error Count'
-            BetterValue = ''
-            IsCritical = $null
-            Description = ''
         },
         [ordered]@{AttributeID = 231
             AttributeIDHex = 'E7'
             AttributeName = 'SSD Life Left'
-            BetterValue = ''
-            IsCritical = $null
-            Description = ''
         },
         [ordered]@{AttributeID = 234
             AttributeIDHex = 'EA'
             AttributeName = 'Unknown SK hynix Attrib'
-            BetterValue = ''
-            IsCritical = $null
-            Description = ''
         },
         [ordered]@{AttributeID = 236
             AttributeIDHex = 'EC'
             AttributeName = 'Unknown SK hynix Attrib'
-            BetterValue = ''
-            IsCritical = $null
-            Description = ''
         },
         [ordered]@{AttributeID = 238
             AttributeIDHex = 'EE'
             AttributeName = 'Unknown SK hynix Attrib'
-            BetterValue = ''
-            IsCritical = $null
-            Description = ''
         },
         [ordered]@{AttributeID = 241
             AttributeIDHex = 'F1'
             AttributeName = 'Total Writes GB'
-            BetterValue = ''
-            IsCritical = $null
-            Description = ''
         },
         [ordered]@{AttributeID = 242
             AttributeIDHex = 'F2'
             AttributeName = 'Total Reads GB'
-            BetterValue = ''
-            IsCritical = $null
-            Description = ''
         },
         [ordered]@{AttributeID = 243
             AttributeIDHex = 'F3'
             AttributeName = 'Total Media Writes'
-            BetterValue = ''
-            IsCritical = $null
-            Description = ''
         },
         [ordered]@{AttributeID = 249
             AttributeIDHex = 'F9'
             AttributeName = 'NAND Writes GiB'
-            BetterValue = ''
-            IsCritical = $null
-            Description = ''
         },
         [ordered]@{AttributeID = 250
             AttributeIDHex = 'FA'
             AttributeName = 'Read Retry Count'
-            BetterValue = ''
-            IsCritical = $null
-            Description = ''
         }
     ) }
 )
