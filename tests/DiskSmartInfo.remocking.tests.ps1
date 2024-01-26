5330584f0cac584e1e323cb77ef5f926aede34f2
sethvs <sethvs@users.noreply.github.com>
2024-01-26 16:54:03 +0300

    Add MSFT_Disk and MSFT_PhysicalDisk pipeline tests

--- tests/DiskSmartInfo.remocking.tests.ps1
+++ tests/DiskSmartInfo.remocking.tests.ps1
@@ -173,6 +173,30 @@ Describe "DiskSmartInfo remocking tests" {
 
         $diskDriveHost2 = New-CimInstance -CimClass $cimClassDiskDrive -Property $diskDrivePropertiesHost2 -ClientOnly
         $diskDriveHost2 | Add-Member -MemberType NoteProperty -Name PSComputerName -Value $computerNames[1] -Force
+
+        $diskPropertiesHost1 = @{
+            Number = 1
+        }
+        $diskPropertiesHost2 = @{
+            Number = 2
+        }
+        $diskHost1 = New-CimInstance -CimClass $cimClassDisk -Property $diskPropertiesHost1 -ClientOnly
+        $diskHost1 | Add-Member -MemberType NoteProperty -Name PSComputerName -Value $computerNames[0] -Force
+
+        $diskHost2 = New-CimInstance -CimClass $cimClassDisk -Property $diskPropertiesHost2 -ClientOnly
+        $diskHost2 | Add-Member -MemberType NoteProperty -Name PSComputerName -Value $computerNames[1] -Force
+
+        $PhysicalDiskPropertiesHost1 = @{
+            DeviceId = 1
+        }
+        $PhysicalDiskPropertiesHost2 = @{
+            DeviceId = 2
+        }
+        $physicalDiskHost1 = New-CimInstance -CimClass $cimClassPhysicalDisk -Property $physicalDiskPropertiesHost1 -ClientOnly
+        $physicalDiskHost1 | Add-Member -MemberType NoteProperty -Name PSComputerName -Value $computerNames[0] -Force
+
+        $physicalDiskHost2 = New-CimInstance -CimClass $cimClassPhysicalDisk -Property $physicalDiskPropertiesHost2 -ClientOnly
+        $physicalDiskHost2 | Add-Member -MemberType NoteProperty -Name PSComputerName -Value $computerNames[1] -Force
     }
 
     Context "ComputerName" {
@@ -472,6 +496,106 @@ Describe "DiskSmartInfo remocking tests" {
             mock Get-CimInstance -MockWith { $diskThresholdsHDD1, $diskThresholdsHDD2, $diskThresholdsSSD1 } -ParameterFilter { $Namespace -eq $namespaceWMI -and $ClassName -eq $classThresholds } -ModuleName DiskSmartInfo
             mock Get-CimInstance -MockWith { $diskDriveHDD1, $diskDriveHDD2, $diskDriveSSD1 } -ParameterFilter { $ClassName -eq $classDiskDrive } -ModuleName DiskSmartInfo
             $diskSmartInfo = $diskDriveHost1, $diskDriveHost2 | Get-DiskSmartInfo
+        }
+
+        It "Returns DiskSmartInfo object" {
+            $diskSmartInfo | Should -HaveCount 2
+            $diskSmartInfo[0].pstypenames[0] | Should -BeExactly 'DiskSmartInfo#ComputerName'
+            $diskSmartInfo[1].pstypenames[0] | Should -BeExactly 'DiskSmartInfo#ComputerName'
+        }
+
+        It "Has ComputerName, Model, and InstanceId properties" {
+            $diskSmartInfo[0].ComputerName | Should -BeIn $computerNames
+            if ($diskSmartInfo[0].ComputerName -eq $computerNames[0])
+            {
+                $diskSmartInfo[0].Model | Should -BeExactly $testsData.Model_HDD2
+                $diskSmartInfo[0].InstanceId | Should -BeExactly $testsData.PNPDeviceID_HDD2
+                $diskSmartInfo[0].SmartData | Should -HaveCount 18
+            }
+            elseif ($diskSmartInfo[0].ComputerName -eq $computerNames[1])
+            {
+                $diskSmartInfo[0].Model | Should -BeExactly $testsData.Model_SSD1
+                $diskSmartInfo[0].InstanceId | Should -BeExactly $testsData.PNPDeviceID_SSD1
+                $diskSmartInfo[0].SmartData | Should -HaveCount 15
+            }
+
+            $diskSmartInfo[1].ComputerName | Should -BeExactly $computerNames.Where{$_ -notlike $diskSmartInfo[0].ComputerName}
+            if ($diskSmartInfo[1].ComputerName -eq $computerNames[0])
+            {
+                $diskSmartInfo[1].Model | Should -BeExactly $testsData.Model_HDD2
+                $diskSmartInfo[1].InstanceId | Should -BeExactly $testsData.PNPDeviceID_HDD2
+                $diskSmartInfo[1].SmartData | Should -HaveCount 18
+            }
+            elseif ($diskSmartInfo[1].ComputerName -eq $computerNames[1])
+            {
+                $diskSmartInfo[1].Model | Should -BeExactly $testsData.Model_SSD1
+                $diskSmartInfo[1].InstanceId | Should -BeExactly $testsData.PNPDeviceID_SSD1
+                $diskSmartInfo[1].SmartData | Should -HaveCount 15
+            }
+        }
+    }
+    Context "MSFT_Disk pipeline" {
+
+        BeforeAll {
+            $cimSessionHost1 = New-MockObject -Type 'Microsoft.Management.Infrastructure.CimSession' -Properties @{ComputerName = $computerNames[0]}
+            $cimSessionHost2 = New-MockObject -Type 'Microsoft.Management.Infrastructure.CimSession' -Properties @{ComputerName = $computerNames[1]}
+            mock New-CimSession -MockWith { $cimSessionHost1, $cimSessionHost2 } -ModuleName DiskSmartInfo
+            mock Remove-CimSession -MockWith { } -ModuleName DiskSmartInfo
+
+            mock Get-CimInstance -MockWith { $diskSmartDataHDD1, $diskSmartDataHDD2, $diskSmartDataSSD1 } -ParameterFilter { $Namespace -eq $namespaceWMI -and $ClassName -eq $classSmartData } -ModuleName DiskSmartInfo
+            mock Get-CimInstance -MockWith { $diskThresholdsHDD1, $diskThresholdsHDD2, $diskThresholdsSSD1 } -ParameterFilter { $Namespace -eq $namespaceWMI -and $ClassName -eq $classThresholds } -ModuleName DiskSmartInfo
+            mock Get-CimInstance -MockWith { $diskDriveHDD1, $diskDriveHDD2, $diskDriveSSD1 } -ParameterFilter { $ClassName -eq $classDiskDrive } -ModuleName DiskSmartInfo
+            $diskSmartInfo = $diskHost1, $diskHost2 | Get-DiskSmartInfo
+        }
+
+        It "Returns DiskSmartInfo object" {
+            $diskSmartInfo | Should -HaveCount 2
+            $diskSmartInfo[0].pstypenames[0] | Should -BeExactly 'DiskSmartInfo#ComputerName'
+            $diskSmartInfo[1].pstypenames[0] | Should -BeExactly 'DiskSmartInfo#ComputerName'
+        }
+
+        It "Has ComputerName, Model, and InstanceId properties" {
+            $diskSmartInfo[0].ComputerName | Should -BeIn $computerNames
+            if ($diskSmartInfo[0].ComputerName -eq $computerNames[0])
+            {
+                $diskSmartInfo[0].Model | Should -BeExactly $testsData.Model_HDD2
+                $diskSmartInfo[0].InstanceId | Should -BeExactly $testsData.PNPDeviceID_HDD2
+                $diskSmartInfo[0].SmartData | Should -HaveCount 18
+            }
+            elseif ($diskSmartInfo[0].ComputerName -eq $computerNames[1])
+            {
+                $diskSmartInfo[0].Model | Should -BeExactly $testsData.Model_SSD1
+                $diskSmartInfo[0].InstanceId | Should -BeExactly $testsData.PNPDeviceID_SSD1
+                $diskSmartInfo[0].SmartData | Should -HaveCount 15
+            }
+
+            $diskSmartInfo[1].ComputerName | Should -BeExactly $computerNames.Where{$_ -notlike $diskSmartInfo[0].ComputerName}
+            if ($diskSmartInfo[1].ComputerName -eq $computerNames[0])
+            {
+                $diskSmartInfo[1].Model | Should -BeExactly $testsData.Model_HDD2
+                $diskSmartInfo[1].InstanceId | Should -BeExactly $testsData.PNPDeviceID_HDD2
+                $diskSmartInfo[1].SmartData | Should -HaveCount 18
+            }
+            elseif ($diskSmartInfo[1].ComputerName -eq $computerNames[1])
+            {
+                $diskSmartInfo[1].Model | Should -BeExactly $testsData.Model_SSD1
+                $diskSmartInfo[1].InstanceId | Should -BeExactly $testsData.PNPDeviceID_SSD1
+                $diskSmartInfo[1].SmartData | Should -HaveCount 15
+            }
+        }
+    }
+    Context "MSFT_PhysicalDisk pipeline" {
+
+        BeforeAll {
+            $cimSessionHost1 = New-MockObject -Type 'Microsoft.Management.Infrastructure.CimSession' -Properties @{ComputerName = $computerNames[0]}
+            $cimSessionHost2 = New-MockObject -Type 'Microsoft.Management.Infrastructure.CimSession' -Properties @{ComputerName = $computerNames[1]}
+            mock New-CimSession -MockWith { $cimSessionHost1, $cimSessionHost2 } -ModuleName DiskSmartInfo
+            mock Remove-CimSession -MockWith { } -ModuleName DiskSmartInfo
+
+            mock Get-CimInstance -MockWith { $diskSmartDataHDD1, $diskSmartDataHDD2, $diskSmartDataSSD1 } -ParameterFilter { $Namespace -eq $namespaceWMI -and $ClassName -eq $classSmartData } -ModuleName DiskSmartInfo
+            mock Get-CimInstance -MockWith { $diskThresholdsHDD1, $diskThresholdsHDD2, $diskThresholdsSSD1 } -ParameterFilter { $Namespace -eq $namespaceWMI -and $ClassName -eq $classThresholds } -ModuleName DiskSmartInfo
+            mock Get-CimInstance -MockWith { $diskDriveHDD1, $diskDriveHDD2, $diskDriveSSD1 } -ParameterFilter { $ClassName -eq $classDiskDrive } -ModuleName DiskSmartInfo
+            $diskSmartInfo = $physicalDiskHost1, $physicalDiskHost2 | Get-DiskSmartInfo
         }
 
         It "Returns DiskSmartInfo object" {
