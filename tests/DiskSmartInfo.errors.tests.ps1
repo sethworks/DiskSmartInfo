8e8b25ec13e549053ed33af87eff25ea4efb6be6
sethvs <sethvs@users.noreply.github.com>
2024-02-05 13:43:45 +0300

    Update tests

--- tests/DiskSmartInfo.errors.tests.ps1
+++ tests/DiskSmartInfo.errors.tests.ps1
@@ -11,24 +11,25 @@ Describe "Errors" {
 
         It "Should throw on two positional parameters" {
 
-            { Get-DiskSmartInfo 'value1' 'value2' } | Should -Throw -ErrorId 'PositionalParameterNotFound,Get-DiskSmartInfo'
+            { Get-DiskSmartInfo 'value1' 'value2' } | Should -Throw "A positional parameter cannot be found that accepts argument 'value2'." -ErrorId 'PositionalParameterNotFound,Get-DiskSmartInfo'
         }
     }
 
-    Context "ComputerName" {
+    Context "Non-existent host" {
+
+        BeforeAll {
+            $nonExistentHost = 'non_existent_host'
+        }
 
         It "Should return error if non-existent host is specified" {
 
-            { Get-DiskSmartInfo 'non_existent_host' -ErrorAction Stop } | Should -Throw -ErrorId 'HRESULT 0x803381b9,Microsoft.Management.Infrastructure.CimCmdlets.NewCimSessionCommand,Get-DiskSmartInfo'
+            { Get-DiskSmartInfo $nonExistentHost -ErrorAction Stop } | Should -Throw "ComputerName: `"$nonExistentHost`"*" -ErrorId 'HRESULT 0x803381b9,Microsoft.Management.Infrastructure.CimCmdlets.NewCimSessionCommand,Get-DiskSmartInfo'
         }
-    }
-
-    Context "CimSession" {
 
         It "Should return error if cim session to non-existent host is specified" {
 
-            $cimSession = New-CimSession -ComputerName 'non_existent_host' -SkipTestConnection
-            { Get-DiskSmartInfo -CimSession $cimSession -ErrorAction Stop } | Should -Throw -ErrorId 'HRESULT 0x803381b9,Microsoft.Management.Infrastructure.CimCmdlets.GetCimInstanceCommand,Get-DiskSmartInfo'
+            $cimSession = New-CimSession -ComputerName $nonExistentHost -SkipTestConnection
+            { Get-DiskSmartInfo -CimSession $cimSession -ErrorAction Stop } | Should -Throw "ComputerName: `"$nonExistentHost`"*" -ErrorId 'HRESULT 0x803381b9,Microsoft.Management.Infrastructure.CimCmdlets.GetCimInstanceCommand,Get-DiskSmartInfo'
         }
     }
 }
