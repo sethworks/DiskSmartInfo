b6ad94d32fc5df597eee5bada3b96ae72aa9d5f7
sethvs <sethvs@users.noreply.github.com>
2024-02-06 14:28:21 +0300

    Add functions folder

--- .helpers/deployModule.ps1
+++ .helpers/deployModule.ps1
@@ -14,6 +14,7 @@ function Deploy-sthModule
     Copy-Item -Path .\* -Include *.psd1, *.psm1, *.ps1, *.ps1xml -Exclude __* -Destination $Path
     # Copy-Item -Path .\Templates -Exclude __* -Destination $Path -Recurse
     Copy-Item -Path .\attributes -Destination $Path -Exclude __* -Recurse
+    Copy-Item -Path .\functions -Destination $Path -Exclude __* -Recurse
     Copy-Item -Path .\tests -Destination $Path -Exclude __* -Recurse
     Copy-Item -Path .\en-us -Destination $Path -Exclude __* -Recurse
     Copy-Item -Path .\ru-ru -Destination $Path -Exclude __* -Recurse
