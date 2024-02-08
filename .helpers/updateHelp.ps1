1894a3b80cb2c1e0100c4908b953efd910983055
sethvs <sethvs@users.noreply.github.com>
2024-02-08 15:05:04 +0300

    Update task

---
+++ .helpers/updateHelp.ps1
@@ -0,0 +1,29 @@
+$docspath = './docs/en-us'
+$helppath = './en-us'
+
+# Generate xml
+New-ExternalHelp -Path $docspath -OutputPath $helppath -Force
+
+# Add two empty lines after all examples except the last
+foreach ($file in (Get-ChildItem -Path $helppath -Filter *.xml).FullName)
+{
+    (Get-Content -Path $file -Raw) `
+        -replace "</maml:para>\r\n        </dev:remarks>\r\n      </command:example>\r\n      <command:example>",
+                 "</maml:para>`n          <maml:para/>`n          <maml:para/>`n        </dev:remarks>`n      </command:example>`n      <command:example>" |
+        Set-Content -Path $file
+}
+
+$docspath = './docs/ru-ru'
+$helppath = './ru-ru'
+
+# Generate xml
+New-ExternalHelp -Path $docspath -OutputPath $helppath -Force
+
+# Add two empty lines after all examples except the last
+foreach ($file in (Get-ChildItem -Path $helppath -Filter *.xml).FullName)
+{
+    (Get-Content -Path $file -Raw) `
+        -replace "</maml:para>\r\n        </dev:remarks>\r\n      </command:example>\r\n      <command:example>",
+                 "</maml:para>`n          <maml:para/>`n          <maml:para/>`n        </dev:remarks>`n      </command:example>`n      <command:example>" |
+        Set-Content -Path $file
+}
