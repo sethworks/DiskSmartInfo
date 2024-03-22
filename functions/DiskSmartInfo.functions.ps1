6b4bb6b1453989506f7a392e7d9a705f3118652e
sethvs <sethvs@users.noreply.github.com>
2024-03-22 19:14:53 +0300

    Cleanup

--- functions/DiskSmartInfo.functions.ps1
+++ functions/DiskSmartInfo.functions.ps1
@@ -195,32 +195,5 @@ function Get-DiskSmartInfo
             # Remove unnecessary System.Management.Automation.Runspaces.RemotingErrorRecord objects from ErrorVariable
             inClearRemotingErrorRecords
         }
-
-
-        # $length = $PSCmdlet.SessionState.PSVariable.GetValue($PSCmdlet.MyInvocation.BoundParameters.ErrorVariable).Count
-        # $value = $PSCmdlet.SessionState.PSVariable.Get($PSCmdlet.MyInvocation.BoundParameters.ErrorVariable).Value
-        # $value.RemoveAt(0)
-        # $PSCmdlet.SessionState.PSVariable.Get($PSCmdlet.MyInvocation.BoundParameters.ErrorVariable).Value.RemoveAt($length - 1)
-
-        # while ($value.ForEach{$PSItem.GetType().FullName} -contains 'System.Management.Automation.Runspaces.RemotingErrorRecord')
-        # {
-            #     $value.ForEach{if ($PSItem.GetType().FullName -eq 'System.Management.Automation.Runspaces.RemotingErrorRecord') {$value.Remove($PSItem); continue}}
-            # }
-
-        # if ($PSCmdlet.MyInvocation.BoundParameters.ErrorVariable)
-        # {
-        #     $value = $PSCmdlet.SessionState.PSVariable.GetValue($PSCmdlet.MyInvocation.BoundParameters.ErrorVariable)
-        #     while ($true)
-        #     {
-        #         $value.ForEach{
-        #             if ($PSItem.GetType().FullName -eq 'System.Management.Automation.Runspaces.RemotingErrorRecord')
-        #             {
-        #                 $value.Remove($PSItem)
-        #                 continue
-        #             }
-        #         }
-        #         break
-        #     }
-        # }
     }
 }
