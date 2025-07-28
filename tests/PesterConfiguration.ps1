d9d2356749e3987aedfef0d527850de04ab5898f
sethvs <sethvs@users.noreply.github.com>
2025-07-28 21:20:07 +0300

    Add PSSession tests

--- tests/PesterConfiguration.ps1
+++ tests/PesterConfiguration.ps1
@@ -11,6 +11,7 @@ $PesterConfiguration.Run.Path = '.\tests\DiskSmartInfo.completers.tests.ps1',
                                 '.\tests\DiskSmartInfo.remoting.CIMSession.mocked.tests.ps1',
                                 '.\tests\DiskSmartInfo.remoting.PSSession.mocked.tests.ps1',
                                 '.\tests\DiskSmartInfo.remoting.SSHSession.mocked.tests.ps1',
+                                '.\tests\DiskSmartInfo.remoting.PSSession.ctl.mocked.tests.ps1',
                                 '.\tests\DiskSmartInfo.config.tests.ps1',
                                 '.\tests\DiskSmartInfo.errors.tests.ps1',
                                 '.\tests\DiskSmartAttributeDescription.tests.ps1'
