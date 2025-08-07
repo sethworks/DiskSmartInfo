526d613653ea3f74b61b1be45fcb8f72a8bac87d
sethvs <sethvs@users.noreply.github.com>
2025-08-07 19:21:10 +0300

    Cleanup

--- functions/DiskSmartInfo.functions.history.ps1
+++ functions/DiskSmartInfo.functions.history.ps1
@@ -65,21 +65,17 @@ function inGetHistoricalData
 
     $fullname = inGetHistoricalDataFileName -computerName $computerName
 
-    # if ($content = Get-Content -Path $fullname -Raw -ErrorAction SilentlyContinue)
     if ($historicalDataFileContent = Get-Content -Path $fullname -Raw -ErrorAction SilentlyContinue)
     {
-        # $converted = ConvertFrom-Json -InputObject $content
         $sourceHostHistoricalData = ConvertFrom-Json -InputObject $historicalDataFileContent
 
         if ($IsCoreCLR)
         {
-            # $timestamp = $converted.TimeStamp
             $timestamp = $sourceHostHistoricalData.TimeStamp
         }
         # Windows PowerShell 5.1 ConvertTo-Json converts DateTime objects differently
         else
         {
-            # $timestamp = $converted.TimeStamp.DateTime
             $timestamp = $sourceHostHistoricalData.TimeStamp.DateTime
         }
 
@@ -88,50 +84,38 @@ function inGetHistoricalData
             HistoricalData = @()
         }
 
-        # foreach ($object in $converted.HistoricalData)
         foreach ($sourceDiskHistoricalData in $sourceHostHistoricalData.HistoricalData)
         {
             $hash = [ordered]@{}
             $attributes = @()
 
-            # $hash.Add('Device', [string]$object.Device)
             $hash.Add('Device', [string]$sourceDiskHistoricalData.Device)
 
-            # if ($object.DiskType -eq 'ATA')
             if ($sourceDiskHistoricalData.DiskType -eq 'ATA')
             {
-                # foreach ($at in $object.SmartData)
                 foreach ($sourceAttribute in $sourceDiskHistoricalData.SmartData)
                 {
                     $attribute = [ordered]@{}
-                    # $attribute.Add('ID', [byte]$at.ID)
                     $attribute.Add('ID', [byte]$sourceAttribute.ID)
 
-                    # if ($at.Data.Count -gt 1)
                     if ($sourceAttribute.Data.Count -gt 1)
                     {
-                        # $attribute.Add('Data', [long[]]$at.Data)
                         $attribute.Add('Data', [long[]]$sourceAttribute.Data)
                     }
                     else
                     {
-                        # $attribute.Add('Data', [long]$at.Data)
                         $attribute.Add('Data', [long]$sourceAttribute.Data)
                     }
 
                     $attributes += [PSCustomObject]$attribute
                 }
             }
-            # elseif ($object.DiskType -eq 'NVMe')
             elseif ($sourceDiskHistoricalData.DiskType -eq 'NVMe')
             {
-                # foreach ($at in $object.SmartData)
                 foreach ($sourceAttribute in $sourceDiskHistoricalData.SmartData)
                 {
                     $attribute = [ordered]@{}
-                    # $attribute.Add('Name', [string]$at.Name)
                     $attribute.Add('Name', [string]$sourceAttribute.Name)
-                    # $attribute.Add('Data', [string]$at.Data)
                     $attribute.Add('Data', [string]$sourceAttribute.Data)
                     $attributes += [PSCustomObject]$attribute
                 }
